7862adfdf2611d97d62edd9b973a9e1a982952af
MartLugt <martvanderlugt@hetnet.nl>
2017-10-04 21:25:57 +0200

    changed record.py: -added google and wit.ai recognition through web-api alone -tested google api, it is fast and accurate. -didn't test wit.ai, as it was down(404 bad request) -removed clutter -audio no longer gets saved to .wav file. removed SendToGoogle.py
    Signed-off-by: MartLugt <martvanderlugt@hetnet.nl>

--- record.py
+++ record.py
@@ -1,15 +1,80 @@
 #!/usr/bin/env python3
 
-import pyaudio  #pyaudio for recording
-import wave     #wave for creating .wav file
-import audioop  #audioOperations for checking volume of sound
+import audioop  # audioOperations for checking volume of sound
+import io
+import os
+import subprocess
+import wave  # wave for creating .wav file
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+import pyaudio  # pyaudio for recording
+
+# TODO: change all mixedCase variable names to lower_case
 
 #Set constants
 FORMAT = pyaudio.paInt16
 RATE = 44100
 BUFFER_SIZE = 1024
 START_COOLDOWN = 9
-STOP_COOLDOWN = 43
+STOP_COOLDOWN = 21
+
+
+def get_wav(frames, format, rate):
+    with io.BytesIO() as file:
+        wa = wave.open(file, 'w')
+        wa.setnchannels(1)
+        wa.setsampwidth(pa.get_sample_size(format))
+        wa.setframerate(rate)
+        wa.writeframes(b''.join(frames))
+        wavValue = file.getvalue()
+        wa.close()
+    return wavValue
+
+
+def get_flac(wav_data):
+    base_path = "C:\\Users\martv\AppData\Local\Programs\Python\Python35\Lib\site-packages\speech_recognition\\"
+    flac_converter = os.path.join(base_path,
+                                  "flac-win32.exe")  # FOR WINDOWS. WHEN PORTING TO PI, CHANGE THE WHOLE WAV TO FLAC CONVERSION
+    process = subprocess.Popen([
+        flac_converter,
+        "--stdout", "--totally-silent",
+        # put the resulting FLAC file in stdout, and make sure it's not mixed with any program output
+        "--best",  # highest level of compression available
+        "-",  # the input FLAC file contents will be given in stdin
+    ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, startupinfo=None)
+    flac_data, stderr = process.communicate(wav_data)
+    return flac_data
+
+
+def get_google(flac_data, rate, language="en-US"):
+    url = "http://www.google.com/speech-api/v2/recognize?{}".format(urlencode({
+        "client": "chromium",
+        "lang": language,
+        "key": "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw",
+    }))
+
+    request = Request(url, data=flac_data, headers={"Content-Type": "audio/x-flac; rate={}".format(rate)})
+    response = urlopen(request, timeout=99)
+    text = response.read().decode("utf-8")
+    return text
+
+
+def get_wit(wav_data, language="en-US"):
+    url = "https://api.wit.ai/speech?v=4102017"
+    if language == "en-US":
+        key = "EZ7V7OXWF7TURDDTFCYAM2DWLBX2OAIT"
+    elif language == "nl-NL":
+        key = "57UW2OZP2T3YMJTQK7CHRBCQM5QGWPNJ"
+    else:
+        key = None
+    print(key)
+    request = Request(url, data=wav_data,
+                      headers={"Authorization": "Bearer {}".format(key), "Content-Type": "audio/wav"})
+    response = urlopen(request, timeout=99)
+
+    text = response.read().decode("utf-8")
+    return text
 
 #First create the PyAudio object
 pa = pyaudio.PyAudio()
@@ -25,7 +90,7 @@ print("First be silent, calibrating silence")
 buffer = stream.read(int(RATE / 2)) #half a second
 threshold = audioop.rms(buffer, pa.get_sample_size(FORMAT)) + 200
 
-print("We are now recording")
+print("We are now recording. Start talking for it to start.")
 
 frames = []
 counterThreshold = 0    #TODO: give this a better name
@@ -37,23 +102,20 @@ while True:     #TODO: make this less fast. Give a cooldown
     buffer = stream.read(BUFFER_SIZE)
     level = audioop.rms(buffer, pa.get_sample_size(FORMAT))
     if level > threshold:
-        print("over")
         counterThreshold1 += 1
         frames.append(buffer)
     else:
-        print("under")
         counterThreshold1 = 0
 
     if counterThreshold1 > START_COOLDOWN:
+        print("Recording Activated")
         while True:
             buffer = stream.read(BUFFER_SIZE)
             frames.append(buffer)
             level = audioop.rms(buffer, pa.get_sample_size(FORMAT))
             if level > threshold:
-                print("OVER")
                 counterThreshold = 0
             else:
-                print("UNDER")
                 counterThreshold += 1       #TODO: cut out empty buffers after break
             if counterThreshold > STOP_COOLDOWN:
                 break
@@ -65,11 +127,8 @@ stream.stop_stream() #Stop and close the stream
 stream.close()
 pa.terminate()       #Destroy the PyAudio object
 
-wa = wave.open("output.wav", 'w')
-wa.setnchannels(1)
-wa.setsampwidth(pa.get_sample_size(FORMAT))
-wa.setframerate(RATE)
-wa.writeframes(b''.join(frames))
-wa.close()
+wav_data = get_wav(frames, FORMAT, RATE)
+flac_data = get_flac(wav_data)
 
-print("Done. Recorded to file output.wav.")
+print(get_google(flac_data, RATE, "en-US"))
+print(get_wit(wav_data))
