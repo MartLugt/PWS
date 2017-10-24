dd10febdf9d149fdeff6a94dd8fc3fbf6bb3b5aa
MartLugt <martvanderlugt@hetnet.nl>
2017-10-24 09:55:50 +0200

    added better query parser. it now works
    Signed-off-by: MartLugt <martvanderlugt@hetnet.nl>

--- query_parser.py
+++ query_parser.py
@@ -1,21 +1,40 @@
-text = "What's the time?".lower()
+text = input().lower()
+
+# Must return the intent with the most confidence and its confidence.
+# Very crude and simple...
+
+def parse(text):
+    # Making a dictionary of intents, which are lists of all its keywords.
+    intents = {"get_time": ["time", "get", "what", "test"],
+               "set_time": ["time", "set", "change"],
+               "get_mood": ["how", "mood", "you", "what"]
+               }
+
+    confidences = {}
+
+    # Calculate percentage of keywords
+    for intent in intents:
+        # populate confidence list.
+        confidences[intent] = 0.0
+        for keyword in intents[intent]:
+            if keyword in text:
+                confidences[intent] += 1/len(intents[intent])
+
+    # Calculate total confidence
+    total_confidence = 0
+    for confidence in confidences.values():
+        total_confidence += confidence
+
+    if total_confidence == 0:
+        print("no match")
+        exit(0)
+
+    # Make confidence 1 total.
+    for intent in confidences:
+        confidences[intent] = confidences[intent]/total_confidence
+
+    # Get key with max value
+    return max(confidences, key=confidences.get), max(confidences.values())
 
 
-def get_max(dic):
-    max_value = max(dic.values())
-    max_keys = [k for k, v in dic.items() if v == max_value]
-    return max_value, max_keys
-
-
-# Need to make a tuple/list with the intent and confidence OR dictionary of all intents with their confidence
-intents = {"time_get": 0, "time_set": 0}
-
-if "time" in text:
-    # bad way to do it, intents with more keywords are now more likely.
-    # Confidence should be percentage. Total sum of all confidences should be 1 or 100.
-    if "what" in text or "get" in text:
-        intents["time_get"] += 1
-    if "set" in text:
-        intents["time_set"] += 1
-
-print(get_max(intents))
+print(parse(text))
