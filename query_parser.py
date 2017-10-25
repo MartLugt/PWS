f4dcff95aa8915448de8d69b87489fd334826f54
MartLugt <martvanderlugt@hetnet.nl>
2017-10-25 15:06:51 +0200

    worked on query parser with weighted keywords BUG: never matches on intent with two '0' keywords

--- query_parser.py
+++ query_parser.py
@@ -9,43 +9,63 @@ text = input().lower()
 
 def parse(text):
     # Making a dictionary of intents, which are lists of all its keywords.
+    # need to add this or that for keywords ea what or get
     intents = {"get_time": {"time": 0, "what": 1, "get": 2},
                "set_time": {"time": 0, "set": 1, "change": 2},
                "get_mood": {"mood": 1, "how are you": 1},
                }
 
-    keyword_buffer = []
     confidences = {}
 
     # Calculate percentage of keywords
     for intent in intents:
         # populate confidence list.
-        confidences[intent]= 0.0
+        confidences[intent]= 0
+
+        print("\n" + intent)
 
         points = 0  # TODO: better names
         matches = 0
+        zero_keywords = 0
+
+        keyword_buffer = []
+        print(keyword_buffer)
 
         for keyword in intents[intent]:
             if keyword in text and keyword not in keyword_buffer:
                 points += intents[intent][keyword]
                 matches += 1
+                confidences[intent] = points/matches
+            if intents[intent][keyword] == 0:
+                zero_keywords += 1
+            keyword_buffer.append(keyword)
+        print("keywords with 0:", str(zero_keywords))
 
-    # Calculate total confidence
-    total_confidence = 0
-    for confidence in confidences.values():
-        total_confidence += confidence
+        zeroes = 0
+        for word in keyword_buffer:
+            print(word)
+            if intents[intent][word] == 0:
+                zeroes += 1
+        print("zeroes:", str(zeroes))
 
-    if total_confidence == 0:
-        print("no match")
-        exit(0)
+        if zeroes != zero_keywords:
+            confidences[intent] = 0
 
-    # Make confidence 1 total.
-    for intent in confidences:
-        confidences[intent] = confidences[intent]/total_confidence
+    # # Calculate total confidence
+    # total_confidence = 0
+    # for confidence in confidences.values():
+    #     total_confidence += confidence
+    #
+    # if total_confidence == 0:
+    #     print("no match")
+    #     exit(0)
+    #
+    # # Make confidence 1 total.
+    # for intent in confidences:
+    #     confidences[intent] = confidences[intent]/total_confidence
+    #
+    # # Get key with max value
+    # return max(confidences, key=confidences.get), max(confidences.values())
+    print(confidences)
 
-    # Get key with max value
-    return max(confidences, key=confidences.get), max(confidences.values())
-
-
-print(parse(text))
-print(parse(text))
+parse(text)
