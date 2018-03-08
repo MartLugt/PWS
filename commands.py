3a3cbb5cacca7b100ba41035d0e935415c8dfc6e
martlugt <martvanderlugt@hetnet.nl>
2018-03-08 16:47:46 +0100

    supposed fix for undoc issue

--- commands.py
+++ commands.py
@@ -58,8 +58,8 @@ def record(text=True, ding=True, full=False, check=True):
         if check:
             if not ttext:
                 play(random.choice(conversation["dont_understand"]))
+                time.sleep(2)
                 record(ding, full)
-
         return ttext
     else:
         frames = stt.record(ding=ding)
@@ -235,7 +235,7 @@ def calendar(text):
 def get_num(text):
     while True:
         res_full = record(full=True, check=False)
-        if res_full is not None:
+        if res_full is not None or res_full is not []:
             for res in res_full["alternative"]:
                 try:
                     return int(res["transcript"])
